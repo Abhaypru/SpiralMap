19ef18c83f62d064bb82eb1549c0b5fac3207895
shouryakhanna <shourya.khanna@inaf.it>
2025-01-26 17:53:25 +0100

    Update dtools.py

--- dtools.py
+++ dtools.py
@@ -1,1 +1,42 @@
 
+
+
+
+def fcount(floc,flist=False,nlist=False):
+	
+	'''
+    NAME: fcount
+
+    PURPOSE: counts the number of files in a given directory
+
+
+    INPUT: file location 
+
+    OUTPUT: number count 
+       
+    HISTORY: October 27, 2022 (INAF Torino)
+    	
+	'''
+	
+	
+	cnt = []
+	
+	for fl in os.listdir(floc):
+		cnt.append(fl)
+		
+	cnt = np.array(cnt)	
+	
+	
+	print(str(cnt.size)+' files in total')
+	
+	if flist:
+		# os.system('ls -lh '+floc)	
+		return cnt
+	elif nlist:
+		return cnt.size	
+	else:
+		os.system('ls -lh '+floc)	
+		return 
+
+
+
