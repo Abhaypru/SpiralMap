5c6c8bedb3ea163af9fd6da335b3ddaca7a91f66
shouryakhanna <shourya.khanna@inaf.it>
2025-06-05 13:25:01 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ nbsphinx_prompt_width = "0"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "path_to_docs": "docs",
     "repository_url": "https://github.com/Abhaypru/SpiralMap",
