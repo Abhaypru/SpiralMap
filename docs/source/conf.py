a8668356a05dd75fee9210ba9bb32302ca86bf8c
shouryakhanna <shourya.khanna@inaf.it>
2025-06-09 23:39:20 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,8 @@
 
 import os 
 import sys
-sys.path.insert(0,os.path.abspath("/Users/shouryapro/Documents/GitHub"))
+# sys.path.insert(0,os.path.abspath("/Users/shouryapro/Documents/GitHub"))
+sys.path.insert(0,os.path.abspath("../../.."))
 
 import SpiralMap
 
