48785a34dab23ab3fa9ac264abe2c80d708cb37e
shouryakhanna <shourya.khanna@inaf.it>
2025-06-09 23:25:02 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,13 @@ release = '1.'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode"
+]
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -30,5 +35,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_baseurl = "https://spiralmap.readthedocs.io/en/latest/"
